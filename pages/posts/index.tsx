257b26b386314f3b837e0da835df6b5e0559bcdf
Ivan Zhevakin <ixvakin@gmail.com>
2022-07-21 21:59:43 +0300

    post pages

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { collection } from 'firebase/firestore'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { db } from '../../app/firebaseApp'
 
 const Posts: NextPage = () => {
-  return <div>Список постов</div>
+  const [posts] = useCollection(collection(db, 'posts'))
+  console.log(posts)
+  return (
+    <div>
+      <h1>Список постов</h1>
+      {posts && (
+        <div>
+          {posts.docs.map((post) => (
+            <div key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.id}</Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Posts
